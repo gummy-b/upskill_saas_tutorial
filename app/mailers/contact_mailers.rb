15497013c62d57897e173dc1df9c63b4b045a7d5
MOGALE M <thadhinsky@gmail.com>
2021-08-03 11:33:40 +0200

    Refactor method name to match view

--- app/mailers/contact_mailers.rb
+++ app/mailers/contact_mailers.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'mcbride.mogale@gmail.com'
     
-    def create_email(name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email 
         @body = body
